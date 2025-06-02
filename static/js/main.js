808516658c434ec811e345d00e2a0c09c30c3b1b
Tzain52 <112386343+Tzain52@users.noreply.github.com>
2025-06-03 01:41:01 +0530

    Updated package.json

--- static/js/main.js
+++ static/js/main.js
@@ -76,7 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
         showLoading();
 
         try {
-            const response = await fetch('/query', {
+            const response = await fetch('/.netlify/functions/app', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
